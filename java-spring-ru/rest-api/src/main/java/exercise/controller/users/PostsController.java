166461c7f417cec17563b7e21a2679bf13716211
KOLTAKOV Aleksey <Aleksey.KOLTAKOV@raiffeisen.ru>
2024-03-27 22:37:22 +0300

    done

--- java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -13,6 +13,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import exercise.model.Post;
 import exercise.Data;
 
+// END
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+    private final List<Post> posts = Data.getPosts();
+
+    // GET /api/users/{id}/posts — список всех постов, написанных пользователем с таким же userId, как id в маршруте
+    @GetMapping("/users/{id}/posts")
+    public List<Post> posts(@PathVariable Integer id) {
+        return posts
+                .stream()
+                .filter(p -> p.getUserId() == id)
+                .toList();
+    }
+
+    // POST /api/users/{id}/posts – создание нового поста, привязанного к пользователю по id.
+    // Код должен возвращать статус 201, тело запроса должно содержать slug, title и body. Обратите внимание, что userId берется из маршрута
+    @PostMapping("/users/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post create(@PathVariable Integer id, @RequestBody Post post) {
+        post.setUserId(id);
+        posts.add(post);
+        return post;
+    }
+}
+
 // BEGIN
 
-// END
+
