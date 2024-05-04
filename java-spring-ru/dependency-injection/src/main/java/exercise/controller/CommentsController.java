9be927fb3c226c5816e30c377b81b5cf70193294
KOLTAKOV Aleksey <Aleksey.KOLTAKOV@raiffeisen.ru>
2024-05-04 12:22:45 +0300

    implement dependency-injection task

--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -19,5 +19,49 @@ import exercise.repository.CommentRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
+    @Autowired
+    private CommentRepository commentRepository;
 
+    //GET /comments — список всех комментариев
+    @GetMapping
+    public List<Comment> index() {
+        return commentRepository.findAll();
+    }
+
+    //GET /comments/{id} – просмотр конкретного комментария
+    @GetMapping("/{id}")
+    public Comment show(@PathVariable long id) {
+        var comment = commentRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(String.format("Comment with id %d not found", id)));
+
+        return comment;
+    }
+
+    //POST /comments – создание нового комментария. При успешном создании возвращается статус 201
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    //PUT /comments/{id} – обновление комментария
+    @PutMapping("/{id}")
+    public void update(@PathVariable long id, @RequestBody Comment comment) {
+        commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Comment with id %d not found", id)));
+
+        comment.setId(id);
+        commentRepository.save(comment);
+    }
+
+    //DELETE /comments/{id} – удаление комментария
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        commentRepository.deleteById(id);
+    }
+
+}
 // END
--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -19,5 +19,52 @@ import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
+    //GET /posts — список всех постов
+    @GetMapping
+    public List<Post> index() {
+        return postRepository.findAll();
+    }
+
+    //GET /posts/{id} – просмотр конкретного поста
+    @GetMapping(path = "/{id}")
+    public Post show(@PathVariable long id) {
+        var post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Post with id %d not found", id)));
+        return post;
+    }
+
+    //POST /posts – создание нового поста. При успешном создании возвращается статус 201
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody Post post) {
+        postRepository.save(post);
+    }
+
+    //PUT /posts/{id} – обновление поста
+    @PutMapping(path = "/{id}")
+    public void update(@PathVariable long id, @RequestBody Post post) {
+        postRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException(String.format("Post with id %d not found", id)));
+
+        post.setId(id);
+        postRepository.save(post);
+    }
+
+    //DELETE /posts/{id} – удаление поста. При удалении поста удаляются все комментарии этого поста
+    //Используйте метод deleteByPostId() в репозитории комментариев для удаления комментариев по id поста.
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable long id) {
+        postRepository.deleteById(id);
+        commentRepository.deleteByPostId(id);
+    }
+
+}
 // END
