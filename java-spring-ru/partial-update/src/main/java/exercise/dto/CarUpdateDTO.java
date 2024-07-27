18a7732a5623ad08c68e3dda9826be12c6adb5f6
KOLTAKOV Aleksey <Aleksey.KOLTAKOV@raiffeisen.ru>
2024-07-27 12:25:38 +0300

    complete partial-update task

--- java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
+++ java-spring-ru/partial-update/src/main/java/exercise/dto/CarUpdateDTO.java
@@ -7,5 +7,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 // BEGIN
+@Getter
+@Setter
+public class CarUpdateDTO {
+    @NotNull
+    private JsonNullable<String> model;
 
+    @NotNull
+    private JsonNullable<String> manufacturer;
+
+    @NotNull
+    private JsonNullable<Integer> enginePower;
+}
 // END
--- java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
+++ java-spring-ru/partial-update/src/main/java/exercise/mapper/CarMapper.java
@@ -12,5 +12,17 @@ import exercise.dto.CarDTO;
 import exercise.model.Car;
 
 // BEGIN
+@Mapper(
+        uses = { JsonNullableMapper.class },
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class CarMapper {
+    public abstract CarDTO map(Car model);
 
+    public abstract Car map(CarCreateDTO dto);
+
+    public abstract void update(CarUpdateDTO dto, @MappingTarget Car model);
+}
 // END
